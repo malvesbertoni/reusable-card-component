ba8f4a658e656d7cdfe297d7cafcdd6746bb50bb
malvesbertoni <malvesbertoni@gmail.com>
2021-03-24 17:40:23 -0300

    Final changes to HTML and CSS

--- src/App.js
+++ src/App.js
@@ -5,34 +5,37 @@ function App() {
     <div className="app-wrapper">
       <div className="card-wrapper">
         <section className="card-header">
-          <img src="/bg-pattern-card.svg" alt="Background pattern" />
+          <img
+            src="/pankaj-patel-_SgRNwAVNKw-unsplash.jpg"
+            alt="Background pattern"
+          />
         </section>
         <section className="card-image">
-          <img src="/image-victor.jpg" alt="User profile" />
+          <img src="/me.png" alt="User profile" />
         </section>
         <section className="card-content">
           <div className="main-info">
             <div className="main-info-title">
-              <h1>Victor Crest</h1>
-              <h2>26</h2>
+              <h1>Mateus Alves</h1>
+              <h2>25</h2>
             </div>
             <div className="main-info-subtitle">
-              <p>London</p>
+              <p>Computer Scientist</p>
             </div>
           </div>
         </section>
         <section className="card-footer">
           <div className="secondary-info">
-            <h1>80K</h1>
-            <p>Followers</p>
+            <h1>BS</h1>
+            <p>2017</p>
           </div>
           <div className="secondary-info">
-            <h1>803K</h1>
-            <p>Likes</p>
+            <h1>MS</h1>
+            <p>2021</p>
           </div>
           <div className="secondary-info">
-            <h1>1.4K</h1>
-            <p>Photos</p>
+            <h1>PhD</h1>
+            <p>????</p>
           </div>
         </section>
       </div>
